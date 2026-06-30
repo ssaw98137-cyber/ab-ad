623e21d93e0de8f87f025122b5642b33fc08c333
ssaw98137-cyber <ssaw98137@gmail.com>
2026-06-30 17:07:21 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Main_Page from "./Main_Page";
 import Login from "./Login";
 
 // export const serverRoute = "http://localhost:8080";
-export const serverRoute = "https://arb-bk-see.onrender.com";
+export const serverRoute = "https://ab-se-7kvo.onrender.com";
 export const token = localStorage.getItem("token");
 function App() {
   return (
